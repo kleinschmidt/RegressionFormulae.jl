3d4bf98acfade725b32ca86e05f1922ad98a6485
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-02-13 13:44:24 +0100

    fulldummy

--- src/RegressionFormula.jl
+++ src/RegressionFormula.jl
@@ -9,6 +9,7 @@ import StatsModels: apply_schema
 const TermTuple = NTuple{N, AbstractTerm} where N
 const Schemas = Union{StatsModels.Schema, StatsModels.FullRank}
 
+include("fulldummy.jl")
 include("power.jl")
 
 end # module
---
+++ src/fulldummy.jl
@@ -0,0 +1,23 @@
+# add some syntax to manually promote to full dummy coding
+fulldummy(t::AbstractTerm) =
+    throw(ArgumentError("can't promote $t (of type $(typeof(t))) to full dummy " *
+                        "coding (only CategoricalTerms)"))
+
+function StatsModels.apply_schema(
+    t::FunctionTerm{typeof(fulldummy)},
+    sch::StatsModels.FullRank,
+    Mod::Type{<:MixedModel},
+)
+    fulldummy(apply_schema.(t.args_parsed, Ref(sch), Mod)...)
+end
+
+function fulldummy(t::CategoricalTerm)
+    new_contrasts = StatsModels.ContrastsMatrix(
+        StatsModels.FullDummyCoding(),
+        t.contrasts.levels,
+    )
+    t = CategoricalTerm(t.sym, new_contrasts)
+end
+
+fulldummy(x) =
+    throw(ArgumentError("fulldummy isn't supported outside of a MixedModel formula"))
