ced30aba46d8e80700d99b7b0667e1df09288fbd
Phillip Alday <me@phillipalday.com>
2021-10-18 17:00:34 -0500

    YASG is part of me now

--- src/RegressionFormulae.jl
+++ src/RegressionFormulae.jl
@@ -4,7 +4,7 @@ using StatsModels
 using Combinatorics
 using Base.Iterators
 
-import StatsModels: apply_schema
+using StatsModels: apply_schema
 
 const TermTuple = NTuple{N, AbstractTerm} where N
 const Schemas = Union{StatsModels.Schema, StatsModels.FullRank}
