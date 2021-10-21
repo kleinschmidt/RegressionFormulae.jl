4152bd8fcc483dea1a86120fe44ffe24101ddea9
Phillip Alday <me@phillipalday.com>
2021-10-21 16:34:26 -0500

    formulae, plural

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,16 @@
-using Documenter, RegressionFormula
+using Documenter, RegressionFormulae
 
 makedocs(;
     pages=[
         "Home" => "index.md",
     ],
-    repo="https://github.com/kleinschmidt/RegressionFormula.jl/blob/{commit}{path}#L{line}",
-    sitename="RegressionFormula.jl",
+    repo="https://github.com/kleinschmidt/RegressionFormulae.jl/blob/{commit}{path}#L{line}",
+    sitename="RegressionFormulae.jl",
     authors=["Dave Kleinschmidt", "Phillip Alday"],
 )
 
 deploydocs(;
-    repo="github.com/kleinschmidt/RegressionFormula.jl",
+    repo="github.com/kleinschmidt/RegressionFormulae.jl",
     devbranch = "main",
     push_preview = true
 )
