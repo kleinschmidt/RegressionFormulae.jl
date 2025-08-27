12947fecd0909cadc5b275aa9e8faf48429ecdae
Phillip Alday <me@phillipalday.com>
2025-08-27 11:50:17 -0500

    add a docstring to fulldummy (#27)
    * add a docstring to fulldummy
    
    * Update Project.toml
    
    * Update Documenter.yml
    
    * Update src/fulldummy.jl
    
    Co-authored-by: Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
    
    ---------
    
    Co-authored-by: Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>

--- src/fulldummy.jl
+++ src/fulldummy.jl
@@ -1,3 +1,13 @@
+"""
+    fulldummy(term::CategoricalTerm)
+
+Assign "contrasts" that include all indicator columns (dummy variables) regardless of whether an 
+intercept column is present.
+
+Note that this generally results in an overparameterized model, but may be valid in regularized models.
+"""
+function fulldummy end
+
 # add some syntax to manually promote to full dummy coding
 fulldummy(t::AbstractTerm) =
     throw(ArgumentError("can't promote $t (of type $(typeof(t))) to full dummy " *
